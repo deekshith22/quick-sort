ae2b53959a97846f45cb63d05dc449fb8607a030
Deekshith Vangala <Deekshith_Vangala@epam.com>
2017-07-12 15:16:13 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,25 @@
 'use strict';
 
 function quickSort(input) {
-
+if(input instanceof Array) {
+    if(input.length === 0){
+        return [];
+    }    
+    var leftArray = [];
+    var rightArray = [];
+    var pivotValue = input[0];
+    for(var i = 1; i < input.length; i++ ){
+        if(input[i] < pivotValue){
+            leftArray.push(input[i]);
+        }
+        else{
+            rightArray.push(input[i]);
+        }
+    }
+    return quickSort(leftArray).concat(pivotValue,quickSort(rightArray));
+    }
+else {
+    throw Error;
+}
 };
 module.exports = quickSort;
